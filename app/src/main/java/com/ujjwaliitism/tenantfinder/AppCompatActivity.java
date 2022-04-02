54220967d7713d901b9c8b6d6f65b65c665a67af
ujjwalarts <ujjwal.kumar2711@gmail.com>
2022-04-02 12:39:16 +0530

    signup and signin page design has been completed

---
+++ app/src/main/java/com/ujjwaliitism/tenantfinder/AppCompatActivity.java
@@ -0,0 +1,8 @@
+package com.ujjwaliitism.tenantfinder;
+
+import android.os.Bundle;
+
+public class AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+}
---
+++ app/src/main/java/com/ujjwaliitism/tenantfinder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ujjwaliitism.tenantfinder;
+
+import android.os.Bundle;
+
+public class MainActivity extends  AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+
+    private void setContentView() {
+    }
+}
